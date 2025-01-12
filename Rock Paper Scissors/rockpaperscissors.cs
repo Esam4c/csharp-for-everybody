d73ce6398852d3d6b33d47345b5fcf79dbbc1017
Esam4c <125660895+Esam4c@users.noreply.github.com>
2025-01-12 08:21:08 +0000

    Update rockpaperscissors.cs

--- Rock Paper Scissors/rockpaperscissors.cs
+++ Rock Paper Scissors/rockpaperscissors.cs
@@ -4,27 +4,66 @@ namespace RockPaperScissors
 {
     class Program
     {
+        enum Move { Rock, Paper, Scissors }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Let's play Rock, Paper, Scissors!");
-            Console.WriteLine("Enter your move (rock, paper, or scissors): ");
-            string playerMove = Console.ReadLine().ToLower();
 
-            // generate a random move for the computer
+            while (true)
+            {
+                Move playerMove = GetPlayerMove();
+                Move computerMove = GetComputerMove();
+
+                Console.WriteLine($"Computer played {computerMove}");
+
+                DetermineWinner(playerMove, computerMove);
+
+                Console.WriteLine("Play again? (yes/no): ");
+                if (Console.ReadLine().ToLower() != "yes")
+                {
+                    break;
+                }
+            }
+        }
+
+        static Move GetPlayerMove()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your move (rock, paper, or scissors): ");
+                string playerInput = Console.ReadLine().ToLower();
+
+                switch (playerInput)
+                {
+                    case "rock":
+                        return Move.Rock;
+                    case "paper":
+                        return Move.Paper;
+                    case "scissors":
+                        return Move.Scissors;
+                    default:
+                        Console.WriteLine("Invalid move. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static Move GetComputerMove()
+        {
             Random rand = new Random();
-            string[] moves = { "rock", "paper", "scissors" };
-            string computerMove = moves[rand.Next(3)];
+            return (Move)rand.Next(3);
+        }
 
-            Console.WriteLine("Computer played " + computerMove);
-
-            // determine the winner
+        static void DetermineWinner(Move playerMove, Move computerMove)
+        {
             if (playerMove == computerMove)
             {
                 Console.WriteLine("It's a tie!");
             }
-            else if (playerMove == "rock" && computerMove == "scissors" ||
-                     playerMove == "paper" && computerMove == "rock" ||
-                     playerMove == "scissors" && computerMove == "paper")
+            else if ((playerMove == Move.Rock && computerMove == Move.Scissors) ||
+                     (playerMove == Move.Paper && computerMove == Move.Rock) ||
+                     (playerMove == Move.Scissors && computerMove == Move.Paper))
             {
                 Console.WriteLine("You win!");
             }
@@ -32,8 +71,7 @@ namespace RockPaperScissors
             {
                 Console.WriteLine("Computer wins!");
             }
-
-            Console.ReadLine();
         }
     }
 }
+
